c303ab69dfbc1d7e84f902e09fd75106c1e10e66
Shaurya Pratap Singh <131666898+ShauryaPratapSingh070408@users.noreply.github.com>
2025-12-04 13:05:14 +0530

    Add Jetpack Compose Material 3 theme with Snowflake brand colors

---
+++ app/src/main/java/com/shaurya/snowflake/presentation/theme/Color.kt
@@ -0,0 +1,26 @@
+package com.shaurya.snowflake.presentation.theme
+
+import androidx.compose.ui.graphics.Color
+
+// Snowflake Brand Colors
+val SnowflakePrimary = Color(0xFF64B5F6)
+val SnowflakePrimaryDark = Color(0xFF42A5F5)
+val SnowflakeAccent = Color(0xFF81D4FA)
+val SnowflakeBackground = Color(0xFFF5F9FC)
+val SnowflakeSurface = Color(0xFFFFFFFF)
+
+// Dark Theme
+val SnowflakeDarkBackground = Color(0xFF121212)
+val SnowflakeDarkSurface = Color(0xFF1E1E1E)
+
+// Frost Vision
+val FrostVisionBlue = Color(0xFF4FC3F7)
+
+// Semantic Colors
+val ErrorRed = Color(0xFFF44336)
+val SuccessGreen = Color(0xFF4CAF50)
+val WarningOrange = Color(0xFFFF9800)
+
+// Overlay
+val OverlayBackground = Color(0xCCFFFFFF) // 80% white
+val OverlayDarkBackground = Color(0xCC1E1E1E) // 80% dark
